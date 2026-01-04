0d65104979110037981ad184f2377d9e00484b1e
Adriano Rodrigues <a.rodrigues.sobrinho4@gmail.com>
2026-01-03 21:55:36 -0300

    Update media-queries

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -1,16 +1,33 @@
+// Inicializa o ScrollReveal com a opção de resetar as animações ao rolar a página
 window.revelar = ScrollReveal({reset:true});
 
-// Animação para o título
+/* Animação para o título 
+ * Duração: 2000ms
+ * Distância: 90px
+ * Origem: Topo
+*/
 revelar.reveal('.efeito-text', {
     duration: 2000,
     distance: '90px',
     origin: 'top',
 });
 
-// Animação para as tasks
+/* Animação para as tasks
+    * Duração: 3000ms
+    * Distância: 1500px
+    * Atraso: 500ms
+    * Origem: Esquerda
+*/
 revelar.reveal('.efeito-tasks', {
     duration: 3000,
     distance: '1500px',
     delay: 500,
     origin: 'left',
 });
+
+revelar.reveal('.content--body', {
+    duration: 3000,
+    distance: '1500px',
+    delay: 500,
+    origin: 'bottom',
+})
