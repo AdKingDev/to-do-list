54464b101fbb152a3c7a9bed195d3758b639ebea
Adriano Rodrigues <a.rodrigues.sobrinho4@gmail.com>
2026-02-24 17:03:50 -0300

    update css

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -1,23 +1,18 @@
 // Inicializa o ScrollReveal com a opção de resetar as animações ao rolar a página
 window.revelar = ScrollReveal({reset:true});
 
-/* Animação para o título 
+/* Animação
  * Duração: 2000ms
  * Distância: 90px
  * Origem: Topo
 */
+
 revelar.reveal('.efeito-text', {
     duration: 2000,
     distance: '90px',
     origin: 'top',
 });
 
-/* Animação para as tasks
-    * Duração: 3000ms
-    * Distância: 1500px
-    * Atraso: 500ms
-    * Origem: Esquerda
-*/
 revelar.reveal('.efeito-tasks', {
     duration: 3000,
     distance: '1500px',
--- assets/js/script.js
+++ assets/js/script.js
@@ -40,17 +40,19 @@ function showValues () {
     for (let i = 0; i < values.length; i++) {
         list.innerHTML += `
         <li>
-        ${values[i].name}
-        <button id="btn-ok" onclick="removeTask('${values[i].name}')"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
-        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
-        </svg></button>
+            ${values[i].name}
+            <button id="btn-ok" onclick="removeTask(${i})">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                </svg>
+            </button>
         </li>
         `;
     }
 }
 
 // Função para remover uma tarefa
-function removeTask (index) {
+function removeTask(index) {
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
 
     values.splice(index, 1);
